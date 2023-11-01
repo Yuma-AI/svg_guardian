07cbe14d83414105319246477d3155f18cc129fd
Alex Beznos <46379451+AlexBeznoss@users.noreply.github.com>
2023-11-01 22:07:20 +0000

    Add sanitize logic

--- ext/svg_guardian/src/lib.rs
+++ ext/svg_guardian/src/lib.rs
@@ -1,12 +1,41 @@
-use magnus::{function, prelude::*, Error, Ruby};
+use magnus::{
+    exception::ExceptionClass, function, gc::register_mark_object, prelude::*,
+    value::Lazy, Error, RModule, Ruby,
+};
+use svg_hush::{data_url_filter, Filter};
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
+static SVG_GUARDIAN_SANITIZE_ERROR: Lazy<ExceptionClass> = Lazy::new(|ruby| {
+    let ex = ruby
+        .class_object()
+        .const_get::<_, RModule>("SvgGuardian")
+        .unwrap()
+        .const_get("SanitizeError")
+        .unwrap();
+    // ensure `ex` is never garbage collected (e.g. if constant is
+    // redefined) and also not moved under compacting GC.
+    register_mark_object(ex);
+    ex
+});
+
+fn sanitize(svg: String) -> Result<String, Error> {
+    let ruby = unsafe { Ruby::get_unchecked() };
+    let mut filter = Filter::new();
+    let mut out = Vec::new();
+    filter.set_data_url_filter(data_url_filter::allow_standard_images);
+
+    if let Err(e) = filter.filter(&mut svg.as_bytes(), &mut out) {
+        return Err(Error::new(
+            ruby.get_inner(&SVG_GUARDIAN_SANITIZE_ERROR),
+            e.to_string(),
+        ));
+    } else {
+        return Ok(String::from_utf8(out).unwrap());
+    };
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("SvgGuardian")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+    module.define_singleton_method("sanitize", function!(sanitize, 1))?;
     Ok(())
 }
--- test/test_svg_guardian.rb
+++ test/test_svg_guardian.rb
@@ -3,11 +3,18 @@
 require "test_helper"
 
 class TestSvgGuardian < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::SvgGuardian::VERSION
+  def test_it_sanitizes_svgs
+    dirty_svg = File.read(File.join(__dir__, "support/files/dirty.xml"))
+    clean_svg = File.read(File.join(__dir__, "support/files/clean.xml"))
+
+    assert_equal clean_svg, SvgGuardian.sanitize(dirty_svg)
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_invalid_svg
+    invalid_svg = File.read(File.join(__dir__, "support/files/invalid.xml"))
+    exception = assert_raises SvgGuardian::SanitizeError do
+      SvgGuardian.sanitize(invalid_svg)
+    end
+    assert_equal "XML parsing error", exception.message
   end
 end
