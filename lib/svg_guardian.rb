afe35f8467fb0470e1a90f66bbed847bde21a5d4
Alex Beznos <alex@yuma.ai>
2023-11-08 02:17:27 +0200

    Fix and bump version

--- lib/svg_guardian.rb
+++ lib/svg_guardian.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+begin
+  # load the precompiled extension file
+  ruby_version = /(\d+\.\d+)/.match(::RUBY_VERSION)
+  require_relative "svg_guardian/#{ruby_version}/svg_guardian"
+rescue LoadError
+  require "svg_guardian/svg_guardian"
+end
+
 require_relative "svg_guardian/version"
-require_relative "svg_guardian/svg_guardian"
 
 module SvgGuardian
   class SanitizeError < StandardError; end
